40025ef18ae04c0f3fbf3fb00bf9a137a8b21631
Prem Rajah <premr@ctuk.ctav.co>
2019-08-23 21:14:45 +0100

    Added post-create conponent Created simple input

--- blog-spa/src/app/app.module.ts
+++ blog-spa/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ blog-spa/src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html'
+})
+export class PostCreateComponent {
+
+  enteredValue = '';
+  newPost = 'NO CONTENT';
+
+  onAddPost() {
+    this.newPost = this.enteredValue;
+  }
+
+}
+
