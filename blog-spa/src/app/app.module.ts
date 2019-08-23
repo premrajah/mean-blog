dee2e817e4c1271332ac3c8fa1ccd51740683264
Prem Rajah <premr@ctuk.ctav.co>
2019-08-23 21:17:16 +0100

    Added angular/material

--- blog-spa/src/app/app.module.ts
+++ blog-spa/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
