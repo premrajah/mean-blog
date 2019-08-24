b0530ac58e2edf48c1c79f4d86dd9570a6da8897
Prem Rajah <premr@ctuk.ctav.co>
2019-08-24 19:17:53 +0100

    Edited serve.js with error handling

--- server.js
+++ server.js
@@ -1,10 +1,50 @@
-const http = require('http');
 const app = require('./backend/app');
+const debug = require("debug")("node-angular");
+const http = require('http');
 
-const port = process.env.PORT || 3000;
+const normalizePort = val => {
+  var port = parseInt(val, 10);
 
-app.set('port', port);
+  if (isNaN(port)) {
+    return val; // named pipe
+  }
+  if (port >= 0) {
+    return port; // port number
+  }
+  return false;
+};
+
+const onError = error => {
+  if (error.svscall !== 'listen') {
+    throw error;
+  }
+
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " Requires elevated privileges.");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " Is already in use.");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+  debug("Listening on " + bind);
+};
+
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
 
 const server = http.createServer(app);
-
+server.on("error", onError);
+server.on("listening", onListening);
 server.listen(port);
