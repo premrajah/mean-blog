51964232142a422fb02defb48547c7f5f632453c
Prem Rajah <premr@ctuk.ctav.co>
2019-08-24 19:27:02 +0100

    created first route /api/posts

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("first");
-  next();
-});
+app.use('/api/posts', (req, res, next) => {
 
-app.use((req, res, next) => {
-  console.log("second");
-  res.send("Hello from express");
+  const posts = [{
+      id: "fadf123421l",
+      title: " First server side post",
+      content: "first server side content"
+    },
+    {
+      id: "dfghjkliiut",
+      title: " second server side post",
+      content: "second server side content"
+    }
+  ];
+
+  res.status(200).json({
+    message: 'Post fetched succesfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
