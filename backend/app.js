e94ed98424a19e66a8def342c72ff04487dfa0eb
Prem Rajah <premr@ctuk.ctav.co>
2019-08-24 18:42:20 +0100

    Changed folder structure (dramatically) Installed express created server.js, app.js

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("first");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("second");
+  res.send("Hello from express");
+});
