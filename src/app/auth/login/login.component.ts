2d16f60039f8a9907dc52ddf1a989f23bee7ea9c
Prem Rajah <premr@ctuk.ctav.co>
2019-08-27 23:14:18 +0100

    Added login edits

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -12,6 +13,10 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onLogin(form: NgForm) {
+    console.log(form.value);
   }
 
 }
