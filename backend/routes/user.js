68143b4dcc343b217c5a5a8c1b887c2e6aebb5bf
Prem <prem.rajah@gmail.com>
2019-08-30 22:57:15 +0100

    installed bcrypt created signup route in backend updated user.js route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,13 +1,41 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+
+const User = require('../models/user');
+
 
 const router = express.Router();
 
 
 
 // register routes
-
-
 router.post("/signup", (req, res, next) => {
+
+  //excrype password
+  bcrypt.hash(req.body.password, 10)
+  .then(hash => {
+
+    const user = new User({
+      email: req.body.email,
+      password: hash
+    });
+
+    user.save()
+    .then(result => {
+      res.status(201).json({
+        message: 'User Created.',
+        result: result
+      })
+
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      });
+    })
+
+  })
+
 
 });
 
