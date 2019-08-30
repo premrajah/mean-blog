ac6ec651a247c47b562d0defb747e074382575f2
Prem <prem.rajah@gmail.com>
2019-08-30 23:44:14 +0100

    Added jsonwebtoken Added route for signup compared passwords with bcrypt Created jwt token and return in response

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -13,33 +14,75 @@ router.post("/signup", (req, res, next) => {
 
   //excrype password
   bcrypt.hash(req.body.password, 10)
-  .then(hash => {
+    .then(hash => {
 
-    const user = new User({
-      email: req.body.email,
-      password: hash
-    });
-
-    user.save()
-    .then(result => {
-      res.status(201).json({
-        message: 'User Created.',
-        result: result
-      })
-
-    })
-    .catch(err => {
-      res.status(500).json({
-        error: err
+      const user = new User({
+        email: req.body.email,
+        password: hash
       });
-    })
 
-  })
+      user.save()
+        .then(result => {
+          res.status(201).json({
+            message: 'User Created.',
+            result: result
+          })
+
+        })
+        .catch(err => {
+          res.status(500).json({
+            error: err
+          });
+        })
+
+    })
 
 
 });
 
 
+router.post('/login', (req, res, next) => {
+      // chekc the email in the db against the request
+      User.findOne({
+          email: req.body.email
+        })
+        .then(user => {
+          if (!user) {
+            return res.status(401).json({
+              message: 'Auth falied'
+            });
+          }
+
+          // found user compare the hash with password
+          return bcrypt.compare(req.body.password, user.password);
+        })
+        .then(result => {
+          if (!result) {
+            return res.status(401).json({
+                message: 'Auth failed'
+              });
+            }
+
+              // valid password
+              const token = jwt.sign({email: user.email, userId: user._id}, 'secret_this_should_be_longer_than_this_but_i_guess_its_fine', {
+                expiresIn: '1h',
+              });
+
+              res.status(200).json({
+                message: 'Retrived user',
+                token: token
+              })
+            })
+          .catch(err => {
+            return res.status(401).json({
+              message: 'Auth failed',
+              error: err
+            });
+          });
+
+        });
 
 
-module.exports = router;
+
+
+      module.exports = router;
